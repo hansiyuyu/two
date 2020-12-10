65fa1f1a158d9a7d1d6ed65b10fca72246df2a2a
hansiyuyu <2051303328@qq.com>
2020-12-10 20:53:13 +0800

    xiugaidaima

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a="0";
-
+        String a = "0";
+        String b = "asdf";
+        String c = "789456";
     }
 }
